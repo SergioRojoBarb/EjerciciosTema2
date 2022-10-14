c5b3d2bb2fef1abb50074d066848f778e830208c
SergioRojoBarb <sergio.rojobarbolla@usp.ceu.es>
2022-10-14 19:12:12 +0200

    woeyf

--- Ejercicio1.0/ejercicio1.py
+++ Ejercicio1.0/ejercicio1.py
@@ -10,3 +10,38 @@ class Punto:
     def __str__(self):
         ch = "(" + str(self.x) + "," + str(self.y) + ")"
         return ch
+
+    def cuadrante(self):
+        
+        cuadrante = None
+
+        if self.x > 0 and self.y > 0:
+            cuadrante = "1er cuadrante"
+
+        elif self.x > 0 and self.y < 0:
+            cuadrante = "4º cuadrante"
+
+        elif self.x < 0 and self.y > 0:
+            cuadrante = "2º cuadrante"
+
+        elif self.x < 0 and self.y < 0:
+            cuadrante = "3er cuadrante"
+
+        elif self.x == 0 and self.y != 0:
+            cuadrante = "En el eje de ordenadas"
+
+        elif self.x != 0 and self.y == 0:
+            cuadrante = "En el eje de abcisas"
+
+        elif self.x == 0 and self.y == 0:
+            cuadrante = "En el origen"
+        
+        return cuadrante
+        
+        
+
+
+
+
+
+
